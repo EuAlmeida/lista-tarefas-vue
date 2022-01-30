5596a29b65af97f64ad790428d98d990d9255c0a
Gabriel Alves <gabriel52.alves@gmail.com>
2022-01-30 11:49:33 -0300

    commit

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>{{ valor }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["valor"],
+};
+</script>
+
+<style>
+header {
+  background-color: rgb(68, 140, 173);
+  color: white;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Rodape.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>Meu rodapé</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+  background-color: rgb(68, 140, 173);
+  color: rgb(255, 255, 255);;  
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  bottom: 0px;
+
+}
+</style>
